38d9a4196c7f9690a0830b0b1b9735fbf7de25e7
Ankit  Khatkar <ankitkhatkar@Ankits-MacBook-Air.local>
2024-04-30 04:29:50 +0530

    stats addition

--- app.py
+++ app.py
@@ -13,24 +13,28 @@ def player_details(player):
     played_matches = data[(data['batter'] == player) | (data['bowler'] == player)]
     total = played_matches['ID'].nunique()
 
-    with cl1:
-        st.metric('Matches Played',str(total))
-
-    as_batsman = data[data['batter']==player]
+    as_batsman = data[data['batter'] == player]
     total_runs = as_batsman['batsman_run'].sum()
-
-    with cl2:
-        st.metric('Total runs as a batsman', str(total_runs))
-
     max_runs_as_batsman = str(as_batsman.groupby('ID')['batsman_run'].sum().sort_values(ascending=False).iloc[0])
-
-    with cl3:
-        st.metric('Highest score as a batsman', str(max_runs_as_batsman))
-
     batter = as_batsman.groupby('ID', as_index=False)['batsman_run'].sum()
     half_centuries = batter[(batter['batsman_run'] >= 50) & (batter['batsman_run'] < 100)].shape[0]
     centuries = batter[batter['batsman_run'] >= 100].shape[0]
     fifty_hundred = f'{half_centuries}/{centuries}'
+
+    as_bowler = data[data['bowler'] == player]
+    total_wickets = as_bowler['isWicketDelivery'].sum()
+    max_wickets = as_bowler.groupby('ID')['isWicketDelivery'].sum().sort_values(ascending=False).iloc[0]
+
+    with cl1:
+        st.metric('Matches Played',str(total))
+        st.metric('Total wickets as a bowler', str(total_wickets))
+
+    with cl2:
+        st.metric('Total runs as a batsman', str(total_runs))
+        st.metric('Max wickets in a match', str(max_wickets))
+
+    with cl3:
+        st.metric('Highest score as a batsman', str(max_runs_as_batsman))
 
     with cl4:
         st.metric('50s/100s', fifty_hundred)
